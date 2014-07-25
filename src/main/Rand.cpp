96fa04c5e80fc80c2366ea9514c31bc19f00e721
Travis Veazey <travisvz@gmail.com>
2014-07-24 22:29:48 -0500

    Rand object now uses 3 independent registers of 32, 31, and 30 bits to (hopefully) achieve insane amounts of unpredictability

--- src/main/Rand.cpp
+++ src/main/Rand.cpp
@@ -1,18 +1,79 @@
 #include "Rand.h"
 
-Rand::Rand(int seed)
+Rand::Rand(uint32_t seed)
 {
-	regA = 0xACE1u;
+	if(0 == seed)
+	{
+		//No seed provided, provide one
+		seed = 0xACE01u; //Nothing special about this value
+	}
+
+	//Seed regA with seed
+	regA = seed;
+	//Shuffle in some entropy
+	for(int i = 0; i < 32; i++)
+	{
+		rand_rega();
+	}
+
+	//Now seed regB with regA
+	regB = regA;
+	//Shuffle in some entropy
+	for(int i = 0; i < 32; i++)
+	{
+		rand_regb();
+	}
+
+	//Now seed regC with regB
+	regC = regB & 0x3FFFFFFFu;
+	//Shuffle in some entropy
+	for(int i = 0; i < 32; i++)
+	{
+		rand_regc();
+	}
 }
 
 int Rand::randbit()
 {
-	unsigned lsb = regA & 1;
+	return rand_rega() ^ rand_regb() ^ rand_regc();
+}
+
+uint8_t Rand::rand_rega()
+{
+	uint8_t lsb = regA & 1;
 	regA >>= 1;
+
 	if(1 == lsb)
 	{
-		regA ^= 0xB400u;
+		regA ^= LFRS_MASK_32;
 	}
 
 	return lsb;
 }
+
+uint8_t Rand::rand_regb()
+{
+	uint8_t lsb = regB & 1;
+	regB >>= 1;
+
+	if(1 == lsb)
+	{
+		regB ^= LFRS_MASK_31;
+	}
+
+	return lsb;
+}
+
+uint8_t Rand::rand_regc()
+{
+	uint8_t lsb = regC & 1;
+	regC >>= 1;
+
+	if(1 == lsb)
+	{
+		regC ^= LFRS_MASK_30;
+	}
+
+	return lsb;
+}
+
--- src/main/Rand.h
+++ src/main/Rand.h
@@ -3,13 +3,29 @@
 
 #include <stdint.h>
 
+#define LFRS_MASK_32 0x800CE000u
+#define LFRS_MASK_31 0x4004F000u
+#define LFRS_MASK_30 0x20065400u
+
 class Rand
 {
 	public:
-		Rand(int seed = 0);
+		//Constructor allows initializing the state
+		Rand(uint32_t seed = 0);
+
+		//Returns a single random bit
 		int randbit();
 	private:
-		uint16_t regA;
+		//Registers for storing generator state
+		//A is 32 bits; B is 31 bits; and C is 30 bits
+		uint32_t regA;
+		uint32_t regB;
+		uint32_t regC;
+
+		//Methods for retrieving each register
+		uint8_t rand_rega();
+		uint8_t rand_regb();
+		uint8_t rand_regc();
 };
 
 #endif

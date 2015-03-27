42574571a103cb5b82f3f268a4071e755b0ee5fd
Travis Veazey <travisvz@gmail.com>
2015-03-26 20:57:23 -0500

    Update Tile.h to Doxygen documentation

--- src/main/map/Tile.h
+++ src/main/map/Tile.h
@@ -1,54 +1,30 @@
 /**
- * File: Tile.h
- * Provides the declaration for the Tile object
- *
- * Version:
- *   1.0
- *
- * Author:
- *   Travis Veazey
- *
- * Date:
- *   2015-03-26
- */
-#ifndef TILE_H_
-#define TILE_H_
-
-/**
- * Class: Tile
- *
  * The base object for map tiles.
  *
  * Tile objects define the type of map tile, contain objects on the tile, and
  * determine how they are displayed.
+ *
+ * @version 1.0
+ * @author Travis Veazey
  */
+#ifndef TILE_H_
+#define TILE_H_
+
 class Tile
 {
-	/**
-	 * Group: Public
-	 */
 	public:
 		/**
-		 * Constructor: Tile
-		 *
 		 * Default constructor for the Tile object.
 		 */
 		Tile();
 
-	/**
-	 * Group: Private
-	 */
 	private:
 		/**
-		 * Variable: blocksLOS
-		 *
 		 * Whether or not this tile blocks line of sight.
 		 */
 		bool _blocksLOS;
 
 		/**
-		 * Variable: blocksMovement
-		 *
 		 * Whether or not this tile blocks movement.
 		 */
 		bool _blocksMovement;

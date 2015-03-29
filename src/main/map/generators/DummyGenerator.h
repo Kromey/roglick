9961730893efa14a0d943311055e8162688c7fc2
Travis Veazey <travisvz@gmail.com>
2015-03-29 16:05:20 -0500

    Documenting Generator and DummyGenerator declarations.

--- src/main/map/generators/DummyGenerator.h
+++ src/main/map/generators/DummyGenerator.h
@@ -16,6 +16,14 @@ namespace map
 	class DummyGenerator : public Generator
 	{
 		public:
+			/**
+			 * Generate a width x height map of WallTiles.
+			 *
+			 * @see Generator::generateMap
+			 * @param width
+			 * @param height
+			 * @param tiles
+			 */
 			void generateMap(uint32_t width, uint32_t height, Tile** tiles);
 	};
 }
--- src/main/map/generators/Generator.h
+++ src/main/map/generators/Generator.h
@@ -17,12 +17,32 @@ namespace map
 	class Generator
 	{
 		public:
+			/**
+			 * Abstract method which must be defined by each generator.
+			 *
+			 * @param width The width of the map to be generated.
+			 * @param height The height of the map.
+			 * @param tiles Array pointer to a 2D array of Tiles.
+			 */
 			virtual void generateMap(uint32_t width, uint32_t height, Tile** tiles) = 0;
 
+			/**
+			 * Set the generator's seed.
+			 *
+			 * @param seed
+			 */
 			void setSeed(uint32_t seed);
 
+			/**
+			 * Get the generator's seed.
+			 *
+			 * @return The seed.
+			 */
 			uint32_t getSeed();
 		protected:
+			/**
+			 * The generator's seed.
+			 */
 			uint32_t _seed;
 	};
 }

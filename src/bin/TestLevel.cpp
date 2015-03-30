66234d1b4e7fed83fae0ec3483564781ce65fa8b
Travis Veazey <travisvz@gmail.com>
2015-03-29 21:11:25 -0500

    TestLevel now prints each generator.

--- src/bin/TestLevel.cpp
+++ src/bin/TestLevel.cpp
@@ -1,12 +1,29 @@
+#include <iostream>
+
 #include "map/Level.h"
+#include "map/generators/WallsGenerator.h"
+#include "map/generators/BigRoomGenerator.h"
 #include "map/generators/CaveGenerator.h"
 
 int main()
 {
-	CaveGenerator gen1;
-	Level l1(40, 80, gen1);
+	std::cout << "WallsGenerator:" << std::endl;
+	WallsGenerator walls;
+	Level lwalls(40, 80, walls);
+	lwalls.printLevel();
+	std::cout << std::endl << std::endl;
 
-	l1.printLevel();
+	std::cout << "BigRoomGenerator:" << std::endl;
+	BigRoomGenerator room;
+	Level lroom(40, 80, room);
+	lroom.printLevel();
+	std::cout << std::endl << std::endl;
+
+	std::cout << "CaveGenerator:" << std::endl;
+	CaveGenerator cave;
+	Level lcave(40, 80, cave);
+	lcave.printLevel();
+	std::cout << std::endl << std::endl;
 
 	return 0;
 }

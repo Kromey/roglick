3e978d690c7754191a56b85d1149e1e853ccb054
Travis Veazey <travisvz@gmail.com>
2015-03-31 15:00:29 -0500

    DungeonBSPGenerator now a functional proof-of-concept.
    The full implementation will put randomly-sized rooms in each section, as well as connect them with hallways.

--- src/main/map/generators/DungeonBSPGenerator.cpp
+++ src/main/map/generators/DungeonBSPGenerator.cpp
@@ -22,25 +22,22 @@ void DungeonBSPGenerator::generateMap(uint32_t width, uint32_t height, Tile** ti
 
 void DungeonBSPGenerator::splitTheDungeon(uint32_t x1, uint32_t y1, uint32_t x2, uint32_t y2, uint32_t splits, Rand& rand, Tile** tiles)
 {
-	std::cout << "x1: " << x1 << "; y1: " << y1 << std::endl;
-	std::cout << "x2: " << x2 << "; y2: " << y2 << std::endl;
-
 	if(0 < splits)
 	{
-		uint32_t dx = 0;
-		uint32_t dy = 0;
-
 		if(0 == rand.randBit())
 		{
-			//dx += rand.randInt(x1 * 1.2, x2 * 0.8);
-			dx = (x1 + x2) / 2;
-		} else {
-			//dy += rand.randInt(y1 * 1.2, y2 * 0.8);
-			dy = (y1 + y2) / 2;
-		}
+			//Splitting the x axis
+			uint32_t new_x = rand.randInt(x1 * 1.2, x2 * 0.8);
 
-		splitTheDungeon(x1+dx, y1+dy, x2, y2, splits-1, rand, tiles);
-		splitTheDungeon(x1, y1, x2-dx, y2-dy, splits-1, rand, tiles);
+			splitTheDungeon(x1, y1, new_x, y2, splits-1, rand, tiles);
+			splitTheDungeon(new_x, y1, x2, y2, splits-1, rand, tiles);
+		} else {
+			//Splitting the x axis
+			uint32_t new_y = rand.randInt(y1 * 1.2, y2 * 0.8);
+
+			splitTheDungeon(x1, y1, x2, new_y, splits-1, rand, tiles);
+			splitTheDungeon(x1, new_y, x2, y2, splits-1, rand, tiles);
+		}
 	} else {
 		//Put a room in here
 		for(uint32_t x = x1 + 1; x < x2 - 1; x++)

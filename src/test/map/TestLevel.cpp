d81eb999a1a6c22b348f6cd4340f22456c6c2ed4
Travis Veazey <travisvz@gmail.com>
2015-04-01 15:25:49 -0500

    New test that Level defaults to WallTiles.

--- src/test/map/TestLevel.cpp
+++ src/test/map/TestLevel.cpp
@@ -2,10 +2,18 @@
 
 #include "gtest/gtest.h"
 #include "map/Level.h"
+#include "map/Tile.h"
+
+TEST(LevelTest, DefaultLevelIsWalls)
+{
+	Level l1(10, 10);
+
+	EXPECT_EQ(WallTile, l1.getTile(5, 5));
+}
 
 TEST(LevelTest, LevelGetTile)
 {
-	Level l1(80, 40);
+	Level l1(10, 10);
 
 	EXPECT_NO_THROW(l1.getTile(5, 5));
 	EXPECT_THROW(l1.getTile(50, 50), std::out_of_range);
@@ -13,7 +21,7 @@ TEST(LevelTest, LevelGetTile)
 
 TEST(LevelTest, LevelArrayAccess)
 {
-	Level l1(80, 40);
+	Level l1(10, 10);
 
 	EXPECT_NO_THROW(l1[5][5]);
 	EXPECT_THROW(l1[50][50], std::out_of_range);

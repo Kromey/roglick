5094de16eb87388fb24c7da9f128fb1ea84b1825
Travis Veazey <travisvz@gmail.com>
2015-04-01 16:48:36 -0500

    Modify Level tests to focus on boundaries.

--- src/test/map/TestLevel.cpp
+++ src/test/map/TestLevel.cpp
@@ -15,15 +15,15 @@ TEST(LevelTest, LevelGetTile)
 {
 	Level l1(10, 10);
 
-	EXPECT_NO_THROW(l1.getTile(5, 5));
-	EXPECT_THROW(l1.getTile(50, 50), std::out_of_range);
+	EXPECT_NO_THROW(l1.getTile(9, 9));
+	EXPECT_THROW(l1.getTile(10, 10), std::out_of_range);
 }
 
 TEST(LevelTest, LevelArrayAccess)
 {
 	Level l1(10, 10);
 
-	EXPECT_NO_THROW(l1[5][5]);
-	EXPECT_THROW(l1[50][50], std::out_of_range);
+	EXPECT_NO_THROW(l1[9][9]);
+	EXPECT_THROW(l1[10][10], std::out_of_range);
 }
 

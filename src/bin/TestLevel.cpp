57ad2323ce01094b1a61f5a9d587822c3e248c62
Travis Veazey <travisvz@gmail.com>
2015-04-01 19:06:56 -0500

    TestLevel now uses filters instead of generators.

--- src/bin/TestLevel.cpp
+++ src/bin/TestLevel.cpp
@@ -2,38 +2,38 @@
 #include <time.h>
 
 #include "map/Level.h"
-#include "map/generators/WallsGenerator.h"
-#include "map/generators/BigRoomGenerator.h"
-#include "map/generators/CaveGenerator.h"
-#include "map/generators/DungeonBSPGenerator.h"
+#include "map/filters/FloodFillFilter.h"
 
 int main()
 {
-	std::cout << "WallsGenerator:" << std::endl;
-	WallsGenerator walls;
+	std::cout << "Default Level:" << std::endl;
 	Level lwalls(40, 80);
 	lwalls.printLevel();
 	std::cout << std::endl << std::endl;
 
-	std::cout << "BigRoomGenerator:" << std::endl;
-	BigRoomGenerator room;
-	Level lroom(40, 80);
-	lroom.printLevel();
+	std::cout << "Flood-filled room:" << std::endl;
+	Level room(40, 80);
+	FloodFillFilter fill;
+	fill.setStart(1,1);
+	fill.setEnd(room.getWidth()-2, room.getHeight()-2);
+	fill.setTile(FloorTile);
+	fill.apply(room);
+	room.printLevel();
 	std::cout << std::endl << std::endl;
 
-	std::cout << "CaveGenerator:" << std::endl;
-	CaveGenerator cave;
-	cave.setSeed(time(NULL));
-	Level lcave(40, 80);
-	lcave.printLevel();
-	std::cout << std::endl << std::endl;
+	//std::cout << "CaveGenerator:" << std::endl;
+	//CaveGenerator cave;
+	//cave.setSeed(time(NULL));
+	//Level lcave(40, 80);
+	//lcave.printLevel();
+	//std::cout << std::endl << std::endl;
 
-	std::cout << "DungeonBSPGenerator:" << std::endl;
-	DungeonBSPGenerator dungeon;
-	dungeon.setSeed(time(NULL));
-	Level ldungeon(40, 80);
-	ldungeon.printLevel();
-	std::cout << std::endl << std::endl;
+	//std::cout << "DungeonBSPGenerator:" << std::endl;
+	//DungeonBSPGenerator dungeon;
+	//dungeon.setSeed(time(NULL));
+	//Level ldungeon(40, 80);
+	//ldungeon.printLevel();
+	//std::cout << std::endl << std::endl;
 
 	return 0;
 }

93ad42d26511baaecff1a64de8cfd1cb46d2e821
Travis Veazey <travisvz@gmail.com>
2015-04-05 15:38:55 -0500

    Make cursor invisible.

--- src/main/display/Screen.cpp
+++ src/main/display/Screen.cpp
@@ -5,10 +5,17 @@
 
 Screen::Screen()
 {
+	//Initialized the screen
 	initscr();
+	//Start with a clean slate
 	clear();
+	//Don't echo entered characters on the screen
 	noecho();
 
+	//Hide the cursor
+	curs_set(false);
+
+	//Get our height and width for later
 	getmaxyx(stdscr, _height, _width);
 }
 

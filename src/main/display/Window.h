b783f0a111aab1ba1e5dd298b5cf59176c258c4d
Travis Veazey <travisvz@gmail.com>
2015-04-07 22:47:59 -0500

    Documenting Window class.

--- src/main/display/Window.h
+++ src/main/display/Window.h
@@ -8,28 +8,94 @@
 class Window
 {
 	public:
+		/**
+		 * Create a new Window with the specified width and height, at 0,0.
+		 *
+		 * @param width
+		 * @param height
+		 */
 		Window(uint32_t width, uint32_t height);
+		/**
+		 * Create a new Window with the specified width and height at x,y.
+		 *
+		 * @param width
+		 * @param height
+		 * @param x
+		 * @param y
+		 */
 		Window(uint32_t width, uint32_t height, uint32_t x, uint32_t y);
 
+		/**
+		 * Get the width of the Window.
+		 *
+		 * @return The width.
+		 */
 		uint32_t getWidth();
+		/**
+		 * Get the height of the Window.
+		 *
+		 * @return The height.
+		 */
 		uint32_t getHeight();
 
+		/**
+		 * Get the x position of the Window.
+		 *
+		 * @return The x coordinate.
+		 */
 		uint32_t getX();
+		/**
+		 * Get the y position of the Window.
+		 *
+		 * @return The y coordinate.
+		 */
 		uint32_t getY();
 
+		/**
+		 * Refresh the Window.
+		 */
 		void refresh();
 
+		/**
+		 * Display the character c at the position x,y in the Window.
+		 *
+		 * @param x
+		 * @param y
+		 * @param c
+		 */
 		void add(uint32_t x, uint32_t y, char c);
+		/**
+		 * Display the string str at the position x,y in the Window.
+		 *
+		 * @param x
+		 * @param y
+		 * @param str
+		 */
 		void add(uint32_t x, uint32_t y, std::string str);
 
+		/**
+		 * Erase the character at the position x,y from the Window.
+		 *
+		 * @param x
+		 * @param y
+		 */
 		void erase(uint32_t x, uint32_t y);
 	private:
+		/**
+		 * Store the Window's width and height.
+		 */
 		uint32_t _width;
 		uint32_t _height;
 
+		/**
+		 * Store the Window's x and y coordinates.
+		 */
 		uint32_t _x;
 		uint32_t _y;
 
+		/**
+		 * Pointer to the Window's ncurses object.
+		 */
 		WINDOW* _win;
 };
 

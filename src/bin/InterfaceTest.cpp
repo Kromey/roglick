1fd677905393de001df28a43e554c0bc86c2af9b
Travis Veazey <travisvz@gmail.com>
2015-04-07 19:39:29 -0500

    Window object partially hacked into InterfaceTest.
    Very fugly hacking-in here, there is still some missing functionality from Window.

--- src/bin/InterfaceTest.cpp
+++ src/bin/InterfaceTest.cpp
@@ -2,6 +2,7 @@
 #include <stdint.h>
 
 #include "display/Screen.h"
+#include "display/Window.h"
 #include "map/Level.h"
 #include "map/filters/DrunkardsWalkFilter.h"
 
@@ -30,16 +31,17 @@ int main()
 		}
 	}
 
-	WINDOW* top = newwin(3, screen_x, 0, 0);
+	//WINDOW* top = newwin(3, screen_x, 0, 0);
+	Window top(screen_x, 3, 0, 0);
 	WINDOW* left = newwin(screen_y-2, 20, 2, 0);
 	//WINDOW* main = newwin(screen_y-2, screen_x-19, 2, 19);
 	WINDOW* main = subwin(map, screen_y-3, screen_x-20, 3, 20);
 
-	wborder(top, '|', '|', '-', '-', '+', '+', '+', '+');
+	//wborder(top, '|', '|', '-', '-', '+', '+', '+', '+');
 	wborder(left, '|', '|', '-', '-', '+', '+', '+', '+');
 	//wborder(main, '|', '|', '-', '-', '+', '+', '+', '+');
 
-	mvwprintw(top, 0, 1, "Message Panel");
+	//mvwprintw(top, 0, 1, "Message Panel");
 	mvwprintw(left, 0, 1, "Stat Panel");
 	//mvwprintw(main, 0, 1, "Main Panel");
 
@@ -50,7 +52,8 @@ int main()
 	uint32_t map_view_max_x = screen_x - 20;
 	mvderwin(main, map_view_y, map_view_x);
 
-	wrefresh(top);
+	//wrefresh(top);
+	top.refresh();
 	wrefresh(left);
 	wrefresh(main);
 
@@ -65,28 +68,32 @@ int main()
 			case KEY_UP:
 				if(0 < map_view_y)
 				{
-					mvwprintw(top, 1, 1, "Up");
+					//mvwprintw(top, 1, 1, "Up");
+					top.add(1, 1, 'U');
 					map_view_y--;
 				}
 				break;
 			case KEY_DOWN:
 				if(map_view_max_y > map_view_y)
 				{
-					mvwprintw(top, 1, 1, "Down");
+					//mvwprintw(top, 1, 1, "Down");
+					top.add(1, 1, 'D');
 					map_view_y++;
 				}
 				break;
 			case KEY_LEFT:
 				if(0 < map_view_x)
 				{
-					mvwprintw(top, 1, 1, "Left");
+					//mvwprintw(top, 1, 1, "Left");
+					top.add(1, 1, 'L');
 					map_view_x--;
 				}
 				break;
 			case KEY_RIGHT:
 				if(map_view_max_x > map_view_x)
 				{
-					mvwprintw(top, 1, 1, "Right");
+					//mvwprintw(top, 1, 1, "Right");
+					top.add(1, 1, 'R');
 					map_view_x++;
 				}
 				break;
@@ -98,6 +105,7 @@ int main()
 
 		mvderwin(main, map_view_y, map_view_x);
 		wrefresh(main);
-		wrefresh(top);
+		//wrefresh(top);
+		top.refresh();
 	}
 }

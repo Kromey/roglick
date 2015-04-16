d8c7f84dbfed44a5b917bfaeb2bc12e81895fc88
Travis Veazey <travisvz@gmail.com>
2015-04-16 17:35:47 -0500

    RandTest will now output 10 floats.
    This is in addition to the visualization of the bits it produces.

--- src/bin/RandTest.cpp
+++ src/bin/RandTest.cpp
@@ -1,11 +1,19 @@
 #include <time.h>
 #include <ncurses.h>
+#include <iostream>
 
 #include "display/Screen.h"
 #include "core/Rand.h"
 
 int main()
 {
+	Rand rand(time(NULL));
+
+	for(int i = 0; i < 10; i++)
+	{
+		std::cout << rand.randFloat() << std::endl;
+	}
+
 	Screen screen;
 
 	int width = screen.getWidth()-2;
@@ -15,7 +23,6 @@ int main()
 	while('q' != input && 'Q' != input)
 	{
 		mvprintw(0, 0, "Rand Output:");
-		Rand rand(time(NULL));
 		for(int x = 0; x < width; x++)
 		{
 			for(int y = 0; y < height; y++)

17105dfba56dca45a369c83e4cd46efa1622deda
Travis Veazey <travisvz@gmail.com>
2015-04-18 12:45:17 -0500

    Update Tile tests to use "is" boolean getters.

--- src/test/map/TestTile.cpp
+++ src/test/map/TestTile.cpp
@@ -6,56 +6,56 @@ TEST(TileTest, WallTileIsOpaque)
 {
 	Tile t1 = WallTile;
 
-	EXPECT_FALSE(t1.getTransparent());
+	EXPECT_FALSE(t1.isTransparent());
 }
 
 TEST(TileTest, WallTileIsInpassable)
 {
 	Tile t1 = WallTile;
 
-	EXPECT_FALSE(t1.getPassable());
+	EXPECT_FALSE(t1.isPassable());
 }
 
 TEST(TileTest, FloorTileIsTransparent)
 {
 	Tile t1 = FloorTile;
 
-	EXPECT_TRUE(t1.getTransparent());
+	EXPECT_TRUE(t1.isTransparent());
 }
 
 TEST(TileTest, FloorTileIsPassable)
 {
 	Tile t1 = FloorTile;
 
-	EXPECT_TRUE(t1.getPassable());
+	EXPECT_TRUE(t1.isPassable());
 }
 
 TEST(TileTest, ClosedDoorTileIsOpaque)
 {
 	Tile t1 = ClosedDoorTile;
 
-	EXPECT_FALSE(t1.getTransparent());
+	EXPECT_FALSE(t1.isTransparent());
 }
 
 TEST(TileTest, ClosedDoorTileIsInpassable)
 {
 	Tile t1 = ClosedDoorTile;
 
-	EXPECT_FALSE(t1.getPassable());
+	EXPECT_FALSE(t1.isPassable());
 }
 
 TEST(TileTest, OpenDoorTileIsTransparent)
 {
 	Tile t1 = OpenDoorTile;
 
-	EXPECT_TRUE(t1.getTransparent());
+	EXPECT_TRUE(t1.isTransparent());
 }
 
 TEST(TileTest, OpenDoorTileIsPassable)
 {
 	Tile t1 = OpenDoorTile;
 
-	EXPECT_TRUE(t1.getPassable());
+	EXPECT_TRUE(t1.isPassable());
 }
 
 TEST(TileTest, FloorEqualsFloor)

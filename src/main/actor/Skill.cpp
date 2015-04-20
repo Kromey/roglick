169b0104effb3d63305c4d846844f01b849f5aff
Travis Veazey <travisvz@gmail.com>
2015-04-19 20:51:34 -0500

    New Skill object.

---
+++ src/main/actor/Skill.cpp
@@ -0,0 +1,42 @@
+#include <cstddef>
+
+#include "actor/Skill.h"
+
+Skill::Skill()
+{
+	_ranks = 0;
+	_parent = NULL;
+}
+
+int Skill::getRanks()
+{
+	return _ranks;
+}
+
+void Skill::setRanks(int ranks)
+{
+	_ranks = ranks;
+}
+
+int Skill::getLevel()
+{
+	int level = _ranks;
+
+	if(NULL != _parent)
+	{
+		level += _parent->getLevel()/2;
+	}
+
+	return level;
+}
+
+Skill* Skill::getParent()
+{
+	return _parent;
+}
+
+void Skill::setParent(Skill* parent)
+{
+	_parent = parent;
+}
+
---
+++ src/main/actor/Skill.h
@@ -0,0 +1,67 @@
+#ifndef SKILL_H_
+#define SKILL_H_
+
+/**
+ * Skill objects represent -- what else? -- characters' skills at performing
+ * various tasks. The Skill object additionally handles tests against skills,
+ * including determining the "degree of success" of a check.
+ */
+class Skill
+{
+	public:
+		/**
+		 * Create a default Skill object with no ranks.
+		 */
+		Skill();
+
+		/**
+		 * Retrieve the number of ranks of the Skill.
+		 *
+		 * NOTE: Ranks are only part of the equation in determining the level
+		 * of a Skill.
+		 * @see getLevel
+		 *
+		 * @return The number of ranks in this Skill.
+		 */
+		int getRanks();
+		/**
+		 * Set the number of ranks in this Skill.
+		 *
+		 * @param ranks
+		 */
+		void setRanks(int ranks);
+
+		/**
+		 * Calculates the level of this Skill, which takes into account all the
+		 * things that go into determining such.
+		 *
+		 * @return The Skill's level.
+		 */
+		int getLevel();
+
+		/**
+		 * Get this Skill's parent Skill.
+		 *
+		 * @return Pointer to the Skill's parent Skill.
+		 */
+		Skill* getParent();
+		/**
+		 * Set this Skill's parent Skill.
+		 *
+		 * @param parent Pointer to this Skill's parent Skill.
+		 */
+		void setParent(Skill* parent);
+
+	private:
+		/**
+		 * Ranks in this current Skill.
+		 */
+		int _ranks;
+
+		/**
+		 * The parent Skill.
+		 */
+		Skill* _parent;
+};
+
+#endif

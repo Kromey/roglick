eb8d96f5b85a643baa8bddd2872e3d3e7e075d60
Travis Veazey <travisvz@gmail.com>
2015-05-04 12:57:27 -0500

    RenderSystem::execute now implemented

--- src/main/entity/systems/RenderSystem.cpp
+++ src/main/entity/systems/RenderSystem.cpp
@@ -15,6 +15,27 @@ void RenderSystem::execute(EntityManager& em)
 	//We can only do anything if we have a Window to do it in
 	if(NULL != _win)
 	{
+		//Let's fetch the ComponentManagers we actually care about
+		PositionManager* pm = (PositionManager*)em.getComponentManager(Position);
+		SpriteManager* sm = (SpriteManager*)em.getComponentManager(Sprite);
+
+		//Now let's get the list of Entities we want to work with
+		EntityList entities = em.getEntities();
+		//In our case, only Entities with a Position...
+		entities = pm->filterEntitiesWithComponent(entities);
+		//...and a Sprite we can render there
+		entities = sm->filterEntitiesWithComponent(entities);
+
+		for(EntityListIt it = entities.begin(); it != entities.end(); ++it)
+		{
+			//Get the components for this Entity
+			PositionComponent pos = pm->getPosition(*it);
+			SpriteComponent sprite = sm->getSprite(*it);
+
+			//Now render the given sprite at the specified position
+			//The add method handles boundary checking for us so we don't have to!
+			_win->add(pos.x, pos.y, sprite.c);
+		}
 	}
 }
 

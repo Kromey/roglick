fa29eaa1a35e920063e1d8bdcc29ec845da7ecd1
Travis Veazey <travisvz@gmail.com>
2015-06-01 16:03:16 -0800

    New tests for * and / operators

--- src/test/core/Scaled.cpp
+++ src/test/core/Scaled.cpp
@@ -46,3 +46,41 @@ TEST(ScaledTest, SubtractInt)
 	EXPECT_EQ(2, (int)s2);
 }
 
+TEST(ScaledTest, BasicMultiplication)
+{
+	Scaled s1(2);
+	Scaled s2(4);
+
+	Scaled s3 = s1 * s2;
+
+	EXPECT_EQ(8, (int)s3);
+}
+
+TEST(ScaledTest, BasicDivision)
+{
+	Scaled s1(6);
+	Scaled s2(3);
+
+	Scaled s3 = s1 / s2;
+
+	EXPECT_EQ(2, (int)s3);
+}
+
+TEST(ScaledTest, MultiplyInt)
+{
+	Scaled s1(2);
+
+	Scaled s2 = s1 * 4;
+
+	EXPECT_EQ(8, (int)s2);
+}
+
+TEST(ScaledTest, DivideInt)
+{
+	Scaled s1(6);
+
+	Scaled s2 = s1 / 3;
+
+	EXPECT_EQ(2, (int)s2);
+}
+

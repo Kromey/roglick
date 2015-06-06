7a1069fc69ab0c3a2872944f369ed165602151dd
Travis Veazey <travisvz@gmail.com>
2015-06-05 16:19:03 -0800

    SkillManager now fully implemented.
    Well, not really "fully" implemented, but close-ish -- it compiles and does its basic stuff, anyway. Next up: Ranks!

---
+++ src/main/entity/managers/SkillManager.cpp
@@ -0,0 +1,8 @@
+#include "entity/managers/SkillManager.h"
+
+const SkillComponent SkillManager::NULL_SKILL = { 0, 0 };
+
+SkillManager::SkillManager()
+{
+}
+
--- src/main/entity/managers/SkillManager.h
+++ src/main/entity/managers/SkillManager.h
@@ -10,9 +10,34 @@
  *
  * Attributes define the basic capabilities of an Entity.
  */
-class AttributeManager :
+class SkillManager :
 	public MultiLookupComponentManager<skill_t, SkillComponent>
 {
+	public:
+		/**
+		 * Default and NULL-equivalent SkillComponent value.
+		 */
+		static const SkillComponent NULL_SKILL;
+
+		/**
+		 * Return a NULL_SKILL SkillComponent
+		 *
+		 * @return NULL_SKILL
+		 */
+		virtual SkillComponent getNullSubComponent() { return NULL_SKILL; };
+
+		/**
+		 * Constructor.
+		 */
+		SkillManager();
+
+		/**
+		 * SkillManager is the ComponentManager sub-class for Skill.
+		 *
+		 * @return The Position enumeration of component_t.
+		 */
+		virtual component_t getComponentType() { return Skill; };
+
 };
 
 #endif

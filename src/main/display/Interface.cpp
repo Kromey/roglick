b32e8e71df5bd64f17a81bdd8770a5c466ca8480
Travis Veazey <travisvz@gmail.com>
2015-06-12 14:30:46 -0800

    Interface can now create and display Windows.

--- src/main/display/Interface.cpp
+++ src/main/display/Interface.cpp
@@ -2,7 +2,7 @@
 
 #include "display/Interface.h"
 
-Interface::Interface()
+Interface::Interface() : _next_window(0)
 {
 	//Initialized the screen
 	initscr();
@@ -23,6 +23,55 @@ Interface::Interface()
 Interface::~Interface()
 {
 	endwin();
+}
+
+Window Interface::addWindow(WindowGeometry geometry)
+{
+	//Just a basic window, no need to do anything fancy here
+	return createWindow(geometry);
+}
+
+void Interface::add(Window win, XYPair pos, char c)
+{
+	WindowMeta win_m = getWindowMeta(win);
+
+	if(NULL == win_m.win)
+	{
+		//This Window doesn't exist
+		return;
+	}
+
+	if(pos.x < win_m.geometry.pos.x && pos.y < win_m.geometry.pos.y)
+	{
+		mvwaddch((WINDOW*)win_m.win, win_m.geometry.pos.y, win_m.geometry.pos.x, c);
+	}
+}
+
+void Interface::add(Window win, XYPair pos, std::string str)
+{
+	WindowMeta win_m = getWindowMeta(win);
+
+	if(NULL == win_m.win)
+	{
+		//This Window doesn't exist
+		return;
+	}
+
+	if(pos.x < win_m.geometry.pos.x && pos.y < win_m.geometry.pos.y)
+	{
+		mvwprintw((WINDOW*)win_m.win, win_m.geometry.pos.y, win_m.geometry.pos.x, str.c_str());
+	}
+}
+
+void Interface::refresh()
+{
+	for(std::vector<WindowMeta>::size_type i = 0; i < _windows.size(); ++i)
+	{
+		if(_windows[i].visible)
+		{
+			wrefresh((WINDOW*)_windows[i].win);
+		}
+	}
 }
 
 XYPair Interface::getScreenSize()
@@ -47,5 +96,53 @@ void Interface::resume()
 	reset_prog_mode();
 	//Refresh the screen to restore contents
 	refresh();
+}
+
+Window Interface::createWindow(WindowGeometry geometry)
+{
+	WindowMeta meta;
+
+	meta.id = _next_window++; //Post-increment returns the "old" value first
+	meta.geometry = geometry;
+	meta.visible = true;
+
+	int width = geometry.size.x;
+	int height = geometry.size.y;
+
+	XYPair screen = getScreenSize();
+
+	if(width == AUTO_SIZE)
+	{
+		//Fill the rest of the screen's width
+		width = screen.x - geometry.pos.x;
+	}
+	if(height == AUTO_SIZE)
+	{
+		//Fill the rest of the screen's height
+		height = screen.y - geometry.pos.y;
+	}
+
+	meta.win = newwin(height, width, geometry.pos.y, geometry.pos.x);
+	//Create sub-windows thusly:
+	//meta.win = subwin(_super_win->_win, _height, _width, _y, _x);
+
+	//Now store the window on our "stack"
+	_windows.push_back(meta);
+
+	return meta.id;
+}
+
+Interface::WindowMeta Interface::getWindowMeta(Window win)
+{
+	for(std::vector<WindowMeta>::size_type i = 0; i < _windows.size(); ++i)
+	{
+		if(_windows[i].id == win)
+		{
+			return _windows[i];
+		}
+	}
+
+	WindowMeta meta = { NULL, win, FULLSCREEN_WINDOW, false };
+	return meta;
 }
 
--- src/main/display/Interface.h
+++ src/main/display/Interface.h
@@ -1,6 +1,9 @@
 #ifndef INTERFACE_H_
 #define INTERFACE_H_
 
+#include <vector>
+#include <string>
+
 #include "display/InterfaceWindow.h"
 
 /**
@@ -27,6 +30,38 @@ class Interface
 		~Interface();
 
 		/**
+		 * Add a new Window to the Interface as described by the supplied
+		 * geometry.
+		 *
+		 * @param geometry
+		 *
+		 * @return The Window that has been created
+		 */
+		Window addWindow(WindowGeometry geometry);
+
+		/**
+		 * Display the character c at the position pos in the Window win.
+		 *
+		 * @param win
+		 * @param pos
+		 * @param c
+		 */
+		void add(Window win, XYPair pos, char c);
+		/**
+		 * Display the string str at the position pos in the Window win.
+		 *
+		 * @param win
+		 * @param pos
+		 * @param c
+		 */
+		void add(Window win, XYPair pos, std::string str);
+
+		/**
+		 * Refresh all (visible) Window%s.
+		 */
+		void refresh();
+
+		/**
 		 * Retrieve the dimensions of the screen.
 		 *
 		 * @return The X and Y dimensions of the screen.
@@ -43,11 +78,41 @@ class Interface
 		void resume();
 
 	private:
+		/**
+		 * A container to hold all of our meta-data describing how our Window%s
+		 * should look and behave.
+		 *
+		 * We use a void* for win so that we can avoid "polluting" client code
+		 * with all the countless global stuffs in the ncurses header.
+		 */
 		typedef struct
 		{
-			Window id;
-			WindowGeometry geometry;
+			void* win; ///< Pointer to the ncurses Window.
+			Window id; ///< Unique ID
+			WindowGeometry geometry; ///< Geometry (position and size)
+			bool visible; ///< True if the Window should be drawn
 		} WindowMeta;
+
+		/**
+		 * Create a new Window with the specified geometry, returning its ID.
+		 *
+		 * @param geometry
+		 *
+		 * @return ID of the created Window.
+		 */
+		Window createWindow(WindowGeometry geometry);
+
+		WindowMeta getWindowMeta(Window win);
+
+		/**
+		 * "Ed Emberly's Big Window List"
+		 */
+		std::vector<WindowMeta> _windows;
+
+		/**
+		 * The ID for the next Window we create.
+		 */
+		Window _next_window;
 };
 
 #endif

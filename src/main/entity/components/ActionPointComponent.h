7a60a56f9f997f11540db6c4d59a3bcd15a4bd42
Travis Veazey <travisvz@gmail.com>
2015-07-06 18:41:03 -0800

    New ActionPointComponent

---
+++ src/main/entity/components/ActionPointComponent.h
@@ -0,0 +1,10 @@
+#ifndef ACTIONPOINTCOMPONENT_H_
+#define ACTIONPOINTCOMPONENT_H_
+
+/**
+ * @class ActionPointComponent
+ * Data structure for an action point component
+ */
+typedef int ActionPointComponent;
+
+#endif

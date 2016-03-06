05c5d5225742aa89cab7ed9e026690d78fff3892
Travis Veazey <travisvz@gmail.com>
2016-03-05 15:59:56 -0900

    Rewrite roglick.py to use ECS

--- roglick.py
+++ roglick.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 
 import roglick.lib.libtcodpy as libtcod
+from roglick.components import PositionComponent,SpriteComponent
+from roglick.systems import RenderSystem
+from roglick.ecs.managers import EntityManager
 
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_HEIGHT/2
+EM = EntityManager()
+PC = EM.create_entity()
+
+pc_pos = PositionComponent(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+pc_sprite = SpriteComponent('@')
+
+EM.set_component(PC, pc_pos)
+EM.set_component(PC, pc_sprite)
 
 libtcod.console_set_custom_font(b'data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, b'python/libtcod tutorial', False)
 
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+render_sys = RenderSystem()
+render_sys.set_entity_manager(EM)
+render_sys.set_console(con)
+
 def handle_keys():
-    global playerx, playery
+    global EM, PC
 
     key = libtcod.console_wait_for_keypress(True)
     if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -25,30 +38,35 @@ def handle_keys():
     elif key.vk == libtcod.KEY_ESCAPE:
         return True  #exit game
 
+    pos = EM.get_component(PC, PositionComponent)
+
     #movement keys
     if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-        playery -= 1
+        pos.y -= 1
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-        playery += 1
+        pos.y += 1
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-        playerx -= 1
+        pos.x -= 1
 
     elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-        playerx += 1
+        pos.x += 1
+
+    EM.set_component(PC, pos)
 
 
 while not libtcod.console_is_window_closed():
     libtcod.console_set_default_foreground(con, libtcod.white)
-    libtcod.console_put_char(con, int(playerx), int(playery), b'@', libtcod.BKGND_NONE)
+    #libtcod.console_put_char(con, int(playerx), int(playery), b'@', libtcod.BKGND_NONE)
+    render_sys.execute()
 
     libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
 
     libtcod.console_flush()
 
     #handle keys and exit game if needed
-    libtcod.console_put_char(con, int(playerx), int(playery), b' ', libtcod.BKGND_NONE)
+    #libtcod.console_put_char(con, int(playerx), int(playery), b' ', libtcod.BKGND_NONE)
     exit = handle_keys()
     if exit:
         break

1ceb58bd6df7d09f5b926d667a793f77c82b95dd
Travis Veazey <travisvz@gmail.com>
2016-03-06 13:14:26 -0900

    RenderSystem will now draw Map object
    As a convenient side effect, the Map being redrawn each frame
    (inefficient, but works) solves the issue of the "trailing @", i.e. no
    more issues with moving leaving behind the glyph in the old location.

--- roglick/systems.py
+++ roglick/systems.py
@@ -9,7 +9,16 @@ class RenderSystem(System):
     def set_console(self, con):
         self._con = con
 
+    def set_map(self, the_map):
+        self._map = the_map
+
     def execute(self):
+        for y in range(self._map.height):
+            for x in range(self._map.width):
+                libtcod.console_put_char(self._con,
+                        x, y,
+                        self._map.tiles[x][y].glyph.encode('UTF-8'),
+                        libtcod.BKGND_NONE)
         for entity, components in self._entity_manager.get_entities_with_components(
                 (PositionComponent,SpriteComponent)):
             libtcod.console_put_char(self._con,

f840081198c763886d762764be00aaaff607b012
Travis Veazey <travisvz@gmail.com>
2016-03-10 11:24:54 -0900

    Use keypad for movement

--- roglick/systems/input.py
+++ roglick/systems/input.py
@@ -7,7 +7,7 @@ from roglick.engine import event
 
 class InputSystem(System):
     def execute(self):
-        key = libtcod.console_wait_for_keypress(True)
+        key = self.get_keypress()
         if key.vk == libtcod.KEY_ENTER and key.lalt:
             #Alt+Enter: toggle fullscreen
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
@@ -17,16 +17,34 @@ class InputSystem(System):
 
         pc = self._entity_manager.pc
 
-        #movement keys
-        if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        # Movement keys
+        # Orthoganal movement
+        if key.vk == libtcod.KEY_KP8:
             event.dispatch(MoveEvent(pc, 0, -1))
-
-        elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        elif key.vk == libtcod.KEY_KP2:
             event.dispatch(MoveEvent(pc, 0, 1))
-
-        elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        elif key.vk == libtcod.KEY_KP4:
             event.dispatch(MoveEvent(pc, -1, 0))
-
-        elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        elif key.vk == libtcod.KEY_KP6:
             event.dispatch(MoveEvent(pc, 1, 0))
+        # Diagonal movement
+        elif key.vk == libtcod.KEY_KP7:
+            event.dispatch(MoveEvent(pc, -1, -1))
+        elif key.vk == libtcod.KEY_KP9:
+            event.dispatch(MoveEvent(pc, 1, -1))
+        elif key.vk == libtcod.KEY_KP1:
+            event.dispatch(MoveEvent(pc, -1, 1))
+        elif key.vk == libtcod.KEY_KP3:
+            event.dispatch(MoveEvent(pc, 1, 1))
+
+    def get_keypress(self):
+        key = libtcod.console_wait_for_keypress(True)
+
+        if not key.pressed:
+            return self.get_keypress()
+
+        # Translate key.c into its character reprsentation
+        if key.vk == libtcod.KEY_CHAR:
+            key.c = chr(key.c)
+        return key
 

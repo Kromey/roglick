4d9fa0a9c4f7c3239bcdd437520bd945884310f5
Travis Veazey <travisvz@gmail.com>
2016-03-10 15:52:40 -0900

    Very thin wrapper around logging
    All it does really is ensure that everything we log goes to the right place and in the right format

---
+++ roglick/logging.py
@@ -0,0 +1,9 @@
+from logging import *
+
+
+basicConfig(
+        filename='runtime.log',
+        level=DEBUG,
+        format='%(asctime)s %(name)-20s %(levelname)-4s: %(message)s',
+        datefmt='%Y%m%d.%H%M%S')
+

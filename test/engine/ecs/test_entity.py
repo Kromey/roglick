52437ab17389a57ee8278d870b4459232ca9b3bf
Travis Veazey <travisvz@gmail.com>
2016-03-11 16:01:38 -0900

    New test for Entity dict keys

--- test/engine/ecs/test_entity.py
+++ test/engine/ecs/test_entity.py
@@ -19,3 +19,27 @@ def test_entity_unequality():
     # ...and different Entities
     assert e1 != e2
 
+def test_entity_dict_index():
+    """Test that our Entity object can be used for dict keys
+
+    By default, user-created objects are hashed based on id, meaning that no
+    two different objects will ever reference the same data if used as dict
+    keys. Therefore, if we can correctly store and retrieve data using
+    different objects but which are logically the same, then we know that our
+    overridden __hash__ method is working correctly and properly enabled dict
+    access.
+    """
+    data = {}
+    for x in range(10):
+        data[Entity(x)] = x
+
+    for x in range(10):
+        assert data[Entity(x)] == x
+
+    # Also ensure we can correctly access and modify Entity-keyed dict data
+    for x in range(10):
+        data[Entity(x)] = 2*x
+
+    for x in range(10):
+        assert data[Entity(x)] == 2*x
+

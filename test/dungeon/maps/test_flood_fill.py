c9a8d34adb9fd8bd8bd20f36ff23ca679eb581e1
Travis Veazey <travisvz@gmail.com>
2016-03-15 08:38:17 -0800

    Add flood fill tests
    These bit me during development, but invisibly because I did not have tests written!

---
+++ test/dungeon/maps/test_flood_fill.py
@@ -0,0 +1,26 @@
+from roglick.dungeon.base import Map,Tile
+from roglick.dungeon import tiles
+
+
+def test_flood_fill_returns_cells_once():
+    m = Map(50,50)
+    for x in range(1,50):
+        for y in range(1,50):
+            m.tiles[x][y] = Tile(**tiles.FloorTile)
+
+    cells = [cell for cell in m.flood_fill(6,6)]
+
+    while 0 < len(cells):
+        cell = cells.pop()
+        assert cell not in cells
+
+def test_flood_fill_fills_area():
+    m = Map(50,50)
+    for x in range(50):
+        for y in range(50):
+            m.tiles[x][y] = Tile(**tiles.FloorTile)
+
+    cells = [cell for cell in m.flood_fill(6,6)]
+
+    assert 50*50 == len(cells)
+

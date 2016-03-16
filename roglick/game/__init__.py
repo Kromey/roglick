cf40c5a4f39d5aeffcd144de2386e34ce7ad8205
Travis Veazey <travisvz@gmail.com>
2016-03-15 16:17:26 -0800

    New GameMaster object
    Initializes and runs the game.

---
+++ roglick/game/__init__.py
@@ -0,0 +1,2 @@
+from .game_master import GameMaster
+
---
+++ roglick/game/game_master.py
@@ -0,0 +1,48 @@
+from roglick.engine.ecs import EntityManager,SystemManager
+from roglick.engine.panels import PanelManager,PanelContext
+from roglick.systems import InputSystem,MovementSystem
+from roglick.world import WorldManager
+from roglick import panels
+
+
+class GameMaster(object):
+    def __init__(self):
+        # We need an EntityManager of course
+        self._entities = EntityManager()
+
+        # We need a world for the game to take place in
+        self._init_world()
+
+        # Systems make everything "go"
+        self._init_systems()
+
+        # And of course we need a way to display it all
+        self._init_display()
+
+        # Last -- but certainly not least -- we'll need event handlers
+        self._register_event_handlers()
+
+    def run(self):
+        self._display.draw_panels()
+        self._systems.execute()
+
+    def _init_world(self):
+        self._world = WorldManager(self._entities)
+
+    def _init_systems(self):
+        self._systems = SystemManager(self._entities)
+
+        # Populate our Systems
+        self._systems.add_system(InputSystem())
+        self._systems.add_system(MovementSystem())
+
+    def _init_display(self):
+        self._display = PanelManager('Ro\'glick')
+
+        self._display.add_panel(
+                panels.MapPanel(
+                    self._entities, self._world, PanelContext.MapScreen))
+
+    def _register_event_handlers(self):
+        pass
+

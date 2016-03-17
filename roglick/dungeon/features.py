489a6ac7d5c97bf2e400b436b97eef3f653f483a
Travis Veazey <travisvz@gmail.com>
2016-03-17 14:58:35 -0800

    Adding initial set of Features

---
+++ roglick/dungeon/features.py
@@ -0,0 +1,5 @@
+"""Argument sets for the Feature constructor to create pre-defined Features"""
+
+StairsDown = {'name': 'Stairs', 'glyph': '>', 'is_passable': True}
+StairsUp = {'name': 'Stairs', 'glyph': '<', 'is_passable': True}
+

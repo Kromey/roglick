f367697ae3d09aacfcabc3fda063e99dba2d7428
Travis Veazey <travisvz@gmail.com>
2016-03-18 19:40:33 -0800

    Debugging: Stairs in red for visibility

--- roglick/dungeon/features.py
+++ roglick/dungeon/features.py
@@ -1,5 +1,7 @@
+from roglick.lib import libtcod
+
 """Argument sets for the Feature constructor to create pre-defined Features"""
 
-StairsDown = {'name': 'Stairs', 'glyph': '>', 'is_passable': True}
-StairsUp = {'name': 'Stairs', 'glyph': '<', 'is_passable': True}
+StairsDown = {'name': 'Stairs', 'glyph': '>', 'is_passable': True, 'color_lit': libtcod.red}
+StairsUp = {'name': 'Stairs', 'glyph': '<', 'is_passable': True, 'color_lit': libtcod.red}
 

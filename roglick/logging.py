7d90afd2defc2d4fce6d8fbd45e691780e2ab074
Travis Veazey <travisvz@gmail.com>
2016-03-19 16:54:06 -0800

    Better logging wrapper
    Paving the way for adding some advanced options to the thing

--- roglick/logging.py
+++ roglick/logging.py
@@ -1,9 +1,15 @@
-from logging import *
+import logging
+from logging import DEBUG,INFO,WARNING,ERROR,CRITICAL
 
 
-basicConfig(
+logging.basicConfig(
         filename='runtime.log',
         level=DEBUG,
         format='%(asctime)s %(name)-20s %(levelname)-4s: %(message)s',
         datefmt='%Y%m%d.%H%M%S')
+
+def getLogger(name):
+    logger = logging.getLogger(name)
+
+    return logger
 

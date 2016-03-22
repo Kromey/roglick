5e905e732626876bc9f7ccd7e625edbe61f6dcb0
Travis Veazey <travisvz@gmail.com>
2016-03-22 15:30:08 -0800

    Fix flood fill test

--- test/dungeon/maps/test_flood_fill.py
+++ test/dungeon/maps/test_flood_fill.py
@@ -1,9 +1,10 @@
 from roglick.dungeon.base import Map,Tile
 from roglick.dungeon import tiles
+from roglick.engine import random
 
 
 def test_flood_fill_returns_cells_once():
-    m = Map(50,50)
+    m = Map(50,50, random.Random())
     for x in range(1,50):
         for y in range(1,50):
             m.tiles[x][y] = Tile(**tiles.FloorTile)
@@ -15,7 +16,7 @@ def test_flood_fill_returns_cells_once():
         assert cell not in cells
 
 def test_flood_fill_fills_area():
-    m = Map(50,50)
+    m = Map(50,50, random.Random())
     for x in range(50):
         for y in range(50):
             m.tiles[x][y] = Tile(**tiles.FloorTile)

da14842f2a8eaef3bbbda71f99cb9598e7299273
Travis Veazey <travisvz@gmail.com>
2016-03-23 11:59:15 -0800

    Rework skill_tree for new JSON structure

--- roglick/skill_tree.py
+++ roglick/skill_tree.py
@@ -1,53 +1,67 @@
 import json
 
 
-SKILL_TREE = {}
+_SKILL_TREE = {}
 
 
 class SkillTreeNode(object):
-    def __init__(self, attr, parent):
+    def __init__(self, name, key, attr, parent):
+        self._name = name
+        self._key = key
         self._attr = attr
         self._parent = parent
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def key(self):
+        return self._key
 
     @property
     def attr(self):
         return self._attr
 
     @property
-    def parent_name(self):
-        return self._parent
-
-    @property
     def parent(self):
         if self._parent is None:
             return None
         else:
-            return SKILL_TREE[self._parent]
+            return _SKILL_TREE[self._parent]
 
     def __repr__(self):
-        parent = self.parent
-        if parent is None:
-            return "{cls}({attr})".format(
-                    cls=self.__class__.__name__,
-                    attr=self.attr)
-        else:
-            return "{cls}({attr}, {parentname}:{parentnode})".format(
-                    cls=self.__class__.__name__,
-                    attr=self.attr,
-                    parentname=self.parent_name,
-                    parentnode=parent)
+        return "{cls}({skill})".format(
+                cls=self.__class__.__name__,
+                skill=self.name)
 
+
+def _make_key(parent, child):
+    child = child.lower().replace(' ', '_')
+    if parent:
+        return parent + '.' + child
+    else:
+        return child
+
+def _process_skill_list(skills, parent_key=None):
+    for skill in skills:
+        key = _make_key(parent_key, skill['name'])
+        _SKILL_TREE[key] = SkillTreeNode(
+                name=skill['name'],
+                key=key,
+                attr=skill['attr'],
+                parent=parent_key)
+        _process_skill_list(skill['child_skills'], key)
 
 def _build_skill_tree():
     with open('data/skills.json') as fh:
         data = json.load(fh)
 
-    for skill in data:
-        SKILL_TREE[skill] = SkillTreeNode(**data[skill])
+    _process_skill_list(data)
 _build_skill_tree()
 
 
 def get(skill):
-    return SKILL_TREE[skill]
+    return _SKILL_TREE[skill]
 
 

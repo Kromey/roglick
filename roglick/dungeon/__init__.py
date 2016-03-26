3e39decfb0f9a0ba6ff09534556a4968e7150e81
Travis Veazey <travisvz@gmail.com>
2016-03-26 13:31:50 -0800

    Refactor terrain loading out of __init__.py

--- roglick/dungeon/__init__.py
+++ roglick/dungeon/__init__.py
@@ -1,28 +1,3 @@
-import json
-
-
-from roglick.engine import colors
-
-
-class Terrain(object):
-    pass
-
-tiles = Terrain()
-features = Terrain()
-
-with open('data/terrain.json') as fh:
-    terrain = json.load(fh)
-
-for tile in terrain['tiles']:
-    name = tile['name'].lower().replace(' ', '_')
-    tile['color'] = getattr(colors, tile['color'])
-    setattr(tiles, name, tile)
-
-for feature in terrain['features']:
-    name = feature['name'].lower().replace(' ', '_')
-    feature['color'] = getattr(colors, feature['color'])
-    setattr(features, name, feature)
-
-
+from .terrain import tiles,features # Must come first, the others rely on it
 from .base import Map,Tile,Feature
 

f48e1c2783dc1cb4dfeae3676584f6f8f78b6b25
Travis Veazey <travisvz@gmail.com>
2016-03-31 16:03:06 -0800

    New classes for file-based objects

---
+++ roglick/engine/file_obj.py
@@ -0,0 +1,46 @@
+import json
+from glob import glob
+
+
+class FileObj(object):
+    def __init__(self, conf_file, obj_key=None):
+        self._data = {}
+        self._load_file(conf_file, obj_key)
+
+    def _load_file(self, conf_file, obj_key=None):
+        with open(conf_file) as fh:
+            data = json.load(fh)
+
+        if obj_key is not None:
+            data = data[obj_key]
+
+        for key,item in self._process_data(data):
+            self._data[key] = item
+
+    def _make_key(self, item):
+        try:
+            return item['name'].lower().replace(' ', '_')
+        except KeyError:
+            return len(self._data)
+
+    def _process_data(self, data):
+        if isinstance(data, dict):
+            key = self._make_key(data)
+            yield key,self._process_item(data)
+        else:
+            for item in data:
+                key = self._make_key(item)
+                yield key,self._process_item(item)
+
+    def _process_item(self, item):
+        return item
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+
+class MultiFileObj(FileObj):
+    def _load_file(self, file_pattern, obj_key=None):
+        for conf_file in glob(file_pattern):
+            super()._load_file(conf_file, obj_key)
+

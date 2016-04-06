fa3d0eaf1160c9991a0fce1c875175c13c5e630d
Travis Veazey <travisvz@gmail.com>
2016-04-05 16:48:40 -0800

    Combat!
    Very repetitive and not very exciting, owing to the dual facts that nobody fights back yet, and nobody actually has any skills yet.

--- roglick/events.py
+++ roglick/events.py
@@ -35,7 +35,7 @@ class MoveEvent(Event):
                 dy=self.dy)
 
 
-class AttackEvent(Event):
+class CombatEvent(Event):
     def __init__(self, entity, defender, x, y):
         super().__init__(entity=entity)
 
@@ -50,6 +50,67 @@ class AttackEvent(Event):
                 d=self.defender,
                 x=self.x,
                 y=self.y)
+
+
+class AttackEvent(CombatEvent):
+    pass
+
+
+class HitEvent(CombatEvent):
+    def __init__(self, entity, defender, x, y, atk_dos):
+        super().__init__(
+                entity=entity,
+                defender=defender,
+                x=x, y=y)
+
+        self.atk_dos = atk_dos
+
+    def __repr__(self):
+        return "{cls}({e}, {d}, {x}, {y}, {a})".format(
+                cls=self.__class__.__name__,
+                e=self.entity,
+                d=self.defender,
+                x=self.x,
+                y=self.y,
+                a=self.atk_dos)
+
+
+class DamageEvent(CombatEvent):
+    def __init__(self, entity, defender, x, y, dmg):
+        super().__init__(
+                entity=entity,
+                defender=defender,
+                x=x, y=y)
+
+        self.dmg = dmg
+
+    def __repr__(self):
+        return "{cls}({e}, {d}, {x}, {y}, {dmg})".format(
+                cls=self.__class__.__name__,
+                e=self.entity,
+                d=self.defender,
+                x=self.x,
+                y=self.y,
+                dmg=self.dmg)
+
+
+class PenetratingDamageEvent(CombatEvent):
+    def __init__(self, entity, defender, x, y, dmg):
+        super().__init__(
+                entity=entity,
+                defender=defender,
+                x=x, y=y)
+
+        self.dmg = dmg
+
+    def __repr__(self):
+        return "{cls}({e}, {d}, {x}, {y}, {dmg})".format(
+                cls=self.__class__.__name__,
+                e=self.entity,
+                d=self.defender,
+                x=self.x,
+                y=self.y,
+                dmg=self.dmg)
 
 
 class ClimbEvent(Event):
--- roglick/systems/combat.py
+++ roglick/systems/combat.py
@@ -1,6 +1,6 @@
 from roglick.engine.ecs import System, exceptions
 from roglick.components import SkillComponent
-from roglick.engine import event
+from roglick.engine import event, random
 from roglick import events
 
 
@@ -10,4 +10,48 @@ class CombatSystem(System):
         atk_check = events.SkillCheckEvent(
                 atk_event.entity, 'melee.swords.short_sword')
         event.dispatch(atk_check)
+
+        if atk_check.result.success:
+            event.dispatch(events.HitEvent(
+                atk_event.entity, atk_event.defender,
+                atk_event.x, atk_event.y, atk_check.result.dos))
+        else:
+            event.dispatch(events.MessageEvent("You missed!"))
+
+    @event.event_handler(events.HitEvent)
+    def hit_handler(self, hit_event):
+        def_check = events.SkillCheckEvent(
+                hit_event.defender, 'melee.shield')
+        event.dispatch(def_check)
+
+        if def_check.result.success:
+            event.dispatch(events.MessageEvent("It blocked your attack!"))
+        else:
+            dmg = self._roll_damage(hit_event.entity, hit_event.atk_dos)
+            event.dispatch(events.DamageEvent(
+                hit_event.entity, hit_event.defender,
+                hit_event.x, hit_event.y, dmg))
+
+    @event.event_handler(events.DamageEvent)
+    def dmg_handler(self, dmg_event):
+        # TODO: This method will get the defender's armor and apply DR
+        # For now, let's just pretend the defender is naked
+        event.dispatch(events.PenetratingDamageEvent(
+            dmg_event.entity, dmg_event.defender,
+            dmg_event.x, dmg_event.y, dmg_event.dmg))
+
+    @event.event_handler(events.PenetratingDamageEvent)
+    def pen_dmg_handler(self, dmg_event):
+        # TODO: This method will apply damage to the defender
+        # For now, any damage is a kill
+        self._entity_manager.destroy_entity(dmg_event.defender)
+        # TODO: We'll actually have another system handle destroying entities
+
+    def _roll_damage(self, attacker, attack_dos):
+        # TODO: Actually get attributes; for now, fudge it and pretend Str=10
+        # NB: This is currently using an alternate damage calculation, which
+        # has not yet been settled upon!
+        dmg = random.roll_dice() * (10 + attack_dos) / 10
+
+        return dmg
 
